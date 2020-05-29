663f1cc2ea150ace070345f9d45dc579135f63cb
notoriousbiggie-code <62277883+notoriousbiggie-code@users.noreply.github.com>
2020-05-29 19:27:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
    <head>
       <title></title>
-      <meta http-equiv = "refresh" content = "0; url = https://blog.thetechmanual.com/review/1.html" />
+      <meta http-equiv = "refresh" content = "0; url = https://blog.thetechmanual.com/r/1.html" />
    </head>
    <body>
       <p></p>
